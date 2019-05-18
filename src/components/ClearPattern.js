87dd8ff490062cd073da331cd6ba97f9ca49c463
louieogrady <l.ogrady@gmail.com>
2019-05-18 13:44:36 +0100

    before merge

--- src/components/ClearPattern.js
+++ src/components/ClearPattern.js
@@ -8,7 +8,7 @@ class ClearPattern extends Component {
 
   render() {
     return (
-      <button class="ui basic button big"  onClick={this.handleClick} style={{position: "absolute", top: "40rem", left: "20.7rem"}}>
+      <button class="ui basic button "  onClick={this.handleClick} style={{position: "absolute", top: "40rem", left: "20.7rem"}}>
       <i class="trash icon" ></i>Clear Pattern
       </button>
     );
